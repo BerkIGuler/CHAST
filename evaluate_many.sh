1516b57af20af751ec73cb7925ddf17aaeafa1d1
Berkay Guler <bierkay1@icluod.com>
2026-03-11 23:22:24 -0700

    Add evaluate_many.sh script to streamline evaluation of multiple TDLA, TDLB, TDLC, TDLD, and TDLE models with specified checkpoints and pilot symbols.

---
+++ evaluate_many.sh
@@ -0,0 +1,11 @@
+python3 evaluate.py --data_path /opt/shared/datasets/NeoRadiumTDLdataset/test/TDLA/ --checkpoint runs/tdla_2/best.pt --pilot_symbols 2
+python3 evaluate.py --data_path /opt/shared/datasets/NeoRadiumTDLdataset/test/TDLB/ --checkpoint runs/tdlb_2/best.pt --pilot_symbols 2
+python3 evaluate.py --data_path /opt/shared/datasets/NeoRadiumTDLdataset/test/TDLC/ --checkpoint runs/tdlc_2/best.pt --pilot_symbols 2
+python3 evaluate.py --data_path /opt/shared/datasets/NeoRadiumTDLdataset/test/TDLD/ --checkpoint runs/tdld_2/best.pt --pilot_symbols 2
+python3 evaluate.py --data_path /opt/shared/datasets/NeoRadiumTDLdataset/test/TDLE/ --checkpoint runs/tdle_2/best.pt --pilot_symbols 2
+
+python3 evaluate.py --data_path /opt/shared/datasets/NeoRadiumTDLdataset/test/TDLA/ --checkpoint runs/tdla_23/best.pt --pilot_symbols 2 3
+python3 evaluate.py --data_path /opt/shared/datasets/NeoRadiumTDLdataset/test/TDLB/ --checkpoint runs/tdlb_23/best.pt --pilot_symbols 2 3
+python3 evaluate.py --data_path /opt/shared/datasets/NeoRadiumTDLdataset/test/TDLC/ --checkpoint runs/tdlc_23/best.pt --pilot_symbols 2 3
+python3 evaluate.py --data_path /opt/shared/datasets/NeoRadiumTDLdataset/test/TDLD/ --checkpoint runs/tdld_23/best.pt --pilot_symbols 2 3
+python3 evaluate.py --data_path /opt/shared/datasets/NeoRadiumTDLdataset/test/TDLE/ --checkpoint runs/tdle_23/best.pt --pilot_symbols 2 3
