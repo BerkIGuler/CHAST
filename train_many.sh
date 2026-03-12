c7d609a3790bc43bb8c55cb28e051ad9d22ff0ab
Berkay Guler <bierkay1@icluod.com>
2026-03-11 17:41:03 -0700

    Add training scripts and configuration files for multiple CHAST models, including TDLA, TDLB, TDLC, and TDLD variants. Each configuration specifies training parameters, dataset paths, and model architecture settings.

---
+++ train_many.sh
@@ -0,0 +1,17 @@
+python3 train.py configs/train_chast_tdla_2.yaml --device cuda:0
+python3 train.py configs/train_chast_tdlb_2.yaml --device cuda:0
+python3 train.py configs/train_chast_tdlc_2.yaml --device cuda:0  
+python3 train.py configs/train_chast_tdld_2.yaml --device cuda:0
+python3 train.py configs/train_chast_tdle_2.yaml --device cuda:0
+
+python3 train.py configs/train_chast_tdla_23.yaml --device cuda:0
+python3 train.py configs/train_chast_tdlb_23.yaml --device cuda:0
+python3 train.py configs/train_chast_tdlc_23.yaml --device cuda:0  
+python3 train.py configs/train_chast_tdld_23.yaml --device cuda:0
+python3 train.py configs/train_chast_tdle_23.yaml --device cuda:0
+
+python3 train.py configs/train_chast_tdla_2711.yaml --device cuda:0
+python3 train.py configs/train_chast_tdlb_2711.yaml --device cuda:0
+python3 train.py configs/train_chast_tdlc_2711.yaml --device cuda:0  
+python3 train.py configs/train_chast_tdld_2711.yaml --device cuda:0
+python3 train.py configs/train_chast_tdle_2711.yaml --device cuda:0
